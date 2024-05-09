612e1b3a0759990a2af13ad4c2309fceaa7e8a55
ashrfras <ashraf.ras@raqmain.net>
2024-05-09 14:16:12 +0100

    Commenting "arguments" with more details

--- optimization/arguments.py
+++ optimization/arguments.py
@@ -3,6 +3,9 @@ import argparse
 def get_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
+    # Larger batch sizes can accelerate the training process by processing more samples in parallel but may require more memory. 
+    # However, smaller batch sizes can provide more accurate gradient estimates and better generalization but may require more iterations to converge.
+    # default=1
     parser.add_argument(
         "--batch_size",
         type=int,
@@ -10,20 +13,32 @@ def get_arguments() -> argparse.Namespace:
         default=1,
     )
 
-    # Diffusion
+    # Skipping steps in the diffusion process refers to bypassing or not performing certain intermediate steps and directly applying the diffusion operation at a later stage. 
+    # This approach is often used to speed up the inference process or reduce computational requirements.
+    # Skipping too many steps may result in a loss of fine-grained details or accuracy, while fewer skipped steps may provide more accurate results but at the cost of increased computation.
+    # default=25
     parser.add_argument(
         "--skip_timesteps",
         type=int,
         help="How many steps to skip during the diffusion.",
         default=25,
     )
+
+    # DDPM = Denoising Diffusion Probabilistic Model
+    # DDIM = Denoising Diffusion Implicit Model
+    # DDIM are a more efficient class of iterative implicit probabilistic models with the same training procedure as DDPMs
+    # Default is DDPM add --ddim if you want to use it
     parser.add_argument(
         "--ddim",
         help="Indicator for using DDIM instead of DDPM",
         action="store_true",
     )
 
-    # For more details read guided-diffusion/guided_diffusion/respace.py
+    # Timestep respacing is a technique used in diffusion models to control the density of sampling during the diffusion process. 
+    # In diffusion models, the goal is to transform an input distribution to a target distribution through a series of iterative steps. 
+    # Timestep respacing refers to adjusting the spacing or density of these steps to achieve specific properties or characteristics in the generated samples.
+    # For more details read https://github.com/openai/guided-diffusion/blob/main/guided_diffusion/respace.py
+    # default=100
     parser.add_argument(
         "--timestep_respacing",
         type=str,
@@ -31,6 +46,8 @@ def get_arguments() -> argparse.Namespace:
         default="100",
     )
 
+    # Add this argument if you do not want to preserve the destination background
+    # Default is enabled, add --no_enforce_background to disable it
     parser.add_argument(
         "--no_enforce_background",
         help="Indicator disabling the last background enforcement",
@@ -38,13 +55,23 @@ def get_arguments() -> argparse.Namespace:
         dest="enforce_background",
     )
 
-    # Augmentations
+    # Data augmentation is a common technique used to artificially increase the diversity and size of the training dataset by applying various transformations or modifications to the existing data.
+    # Increasing the number of augmentations can help improve the model's generalization ability by exposing it to a wider range of variations and reducing the risk of overfitting.
+    # default=8
     parser.add_argument("--aug_num", type=int, help="The number of augmentation", default=8)
 
-    # Misc
+    # Random seed default=404
     parser.add_argument("--seed", type=int, help="The random seed", default=404)
+
+    # The GPU ID default=0
     parser.add_argument("--gpu_id", type=int, help="The GPU ID", default=0)
+
+    # The output path, default=output
+    # folder will be created if doesn't exist
     parser.add_argument("--output_path", type=str, default="output")
+
+    # The output result file
+    # default=output.png
     parser.add_argument(
         "-o",
         "--output_file",
@@ -52,7 +79,16 @@ def get_arguments() -> argparse.Namespace:
         help="The filename to save, must be png",
         default="output.png",
     )
+
+    # Increasing the number of iterations allows for more fine-grained transformations and can potentially capture more details in the generated samples.
+    # However, it also increases the computational cost and may require more memory.
+    # default=4
     parser.add_argument("--iterations_num", type=int, help="The number of iterations", default=4)
+
+    # In the context of DDPM, the term "masking threshold" is not a commonly used concept.
+    # It is not standard in face parsing, nor in Gaze Estimation.
+    # I'm still trying to understand where it is used in this project.
+    # default=30
     parser.add_argument("--masking_threshold", type=int, help="The number of masking threshold", default=30)
 
     args = parser.parse_args()
