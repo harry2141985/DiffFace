543258799dc997879ad8aeb590469e91ef62bff3
Ashraf Ras <ashraf.ras@raqmain.net>
2024-05-10 19:28:23 +0100

    Commenting the code

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from optimization.image_editor import ImageEditor
 from optimization.arguments import get_arguments
 
 if __name__ == "__main__":
+    
+    # we get the arguments collected in optimization/arguments.py
     args = get_arguments()
+    
+    # We make the output dirs
+    # We set defaults for guided diffusion image training
+    # We load checkpoint models (model.pt, arcface, faceparser and GazeEstimator)
     image_editor = ImageEditor(args)
+
+    # This is the main function
     image_editor.edit_image_by_prompt()
