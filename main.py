4ba2e80154c518526a3eb5a5c7e6bc3c9e6eed99
ashrfras <ashraf.ras@raqmain.net>
2024-05-12 22:58:17 +0000

    Adding support for merging back to original image (testing step)

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     # We make the output dirs
     # We set defaults for guided diffusion image training
     # We load checkpoint models (model.pt, arcface, faceparser and GazeEstimator)
-    #image_editor = ImageEditor(args)
+    image_editor = ImageEditor(args)
 
     # This is the main function
-    #image_editor.edit_image_by_prompt()
+    image_editor.edit_image_by_prompt()
 
     # merge
     #merged_image = merge_faces("./data/targ/001.png", "./output/0_0.png", "./output/result.png")
